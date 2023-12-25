09e7b70dc3324479001e2c8e196b88f9aa0d5edb
Patryk <patrykwidlazb@gmail.com>
2023-12-25 16:00:16 +0100

    wip. created simple form, changed display of entities

--- src/Crud components/Comment.tsx
+++ src/Crud components/Comment.tsx
@@ -1,4 +1,5 @@
-import { useState, useEffect } from "react";
+import React, { ChangeEvent, FormEvent, useState, useEffect } from "react";
+import "../Crud components style/Entity.css";
 
 interface Comment {
   postId: number;
@@ -11,6 +12,25 @@ interface Comment {
 function Comment() {
   console.log("sr");
   const [comments, setComments] = useState<Comment[]>([]);
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    body: "",
+  });
+
+  const handleChange = (
+    event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = event.target;
+    setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
+  };
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    alert(
+      `Name: ${formData.name}, Email: ${formData.email}, Body: ${formData.body}`
+    );
+  };
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/comments")
@@ -19,18 +39,50 @@ function Comment() {
   }, []);
 
   return (
-    <div>
-      <h2>Comments</h2>
-      <ul>
+    <>
+      <div id="create-form">
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+          />
+
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+          />
+
+          <label htmlFor="body">Body:</label>
+          <textarea
+            id="body"
+            name="body"
+            value={formData.body}
+            onChange={handleChange}
+          />
+
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+
+      <div id="entity-container">
+        <h2>Existing comments</h2>
         {comments.map((comment) => (
-          <li key={comment.id}>
+          <div key={comment.id} className="single-entity">
             <h3>{comment.name}</h3>
             <p>Email: {comment.email}</p>
             <p>{comment.body}</p>
-          </li>
+          </div>
         ))}
-      </ul>
-    </div>
+      </div>
+    </>
   );
 }
 
