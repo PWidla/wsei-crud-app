2b869c1cc90e8ada32a30f24a26102985fbb5566
Patryk <patrykwidlazb@gmail.com>
2023-12-26 18:28:09 +0100

    temp change. show comments after successful login

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import Header from "./Layout components/Header";
 import Main from "./Layout components/Main";
@@ -9,28 +9,31 @@ function App() {
   let entities = ["Posts", "Comments"];
 
   const [selectedEntity, setSelectedEntity] = useState<string | null>(null);
+  const [loggedInUser, setLoggedInUser] = useState<User | null>(null);
 
   const handleSelectEntity = (entity: string) => {
     setSelectedEntity(entity);
   };
 
-  const [loggedInUser, setLoggedInUser] = useState<User | null>(null);
-
   const handleLogin = (user: User) => {
     setLoggedInUser(user);
   };
 
+  useEffect(() => {
+    if (entities.length > 0) {
+      setSelectedEntity(entities[0]);
+    }
+  }, [loggedInUser]);
+
   return (
     <>
-      {!loggedInUser ? (
-        <>
-          <Login onLogin={handleLogin} />
-        </>
-      ) : (
+      {loggedInUser ? (
         <>
           <Header entities={entities} onSelectEntity={handleSelectEntity} />
           <Main selectedEntity={selectedEntity} />
         </>
+      ) : (
+        <Login onLogin={handleLogin} />
       )}
     </>
   );
